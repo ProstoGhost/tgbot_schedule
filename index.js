80995c27925d21c5796f711ab2358e3806a6d36f
ProstoGhost <tam_1910@bk.ru>
2023-02-16 18:02:42 +0500

    added teacher search button

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ENV = require('dotenv').config();
 const TelegramAPI = require('node-telegram-bot-api');
 
 const Group = require('./schedule.json');
-const {Weekoptions} = require('./options')
+const {Weekoptions, LowwerKeyboard, TeacherList} = require('./options')
 
 
 const bot = new TelegramAPI(process.env.TOKEN,{polling: true})
@@ -36,18 +36,23 @@ const start = () => {
     //CurrentTask[chatId] = 0
 
     if(text === '/start' & CurrentTask[chatId] == null){
-      return bot.sendMessage(chatId, `Данный бот выдаёт расписание вашей группы`)
+      return bot.sendMessage(chatId, `Данный бот выдаёт расписание вашей группы`, LowwerKeyboard)
     }
     if(text === '/info' & CurrentTask[chatId] == null){
       return bot.sendMessage(chatId, `Напишите в сообщении название вашей группы(Заглавными буквами) и выберите день`)
     }
-    if (text === `/schedule` & CurrentTask[chatId] == null) {
-      bot.sendMessage(chatId, `Напишите номер группы или напишите "Отмена" для отмены поиска`);
+    if ((text === `/schedule`|| text === `Рассписание`) & CurrentTask[chatId] == null) {
+      bot.sendMessage(chatId, `Напишите номер группы или "Отмена" для возврата в главное меню`);
       CurrentTask[chatId] = 1;
       return;
     }
+    if ((text === `/teacher`|| text === `Преподователи`) & CurrentTask[chatId] == null) {
+      bot.sendMessage(chatId, `Выберите преподователя чтобы узнать его почту или напишите "Отмена" для возврата в главное меню`, TeacherList);
+      CurrentTask[chatId] = 2;
+      return;
+    }
     if (text.toLocaleLowerCase() === 'отмена' & CurrentTask[chatId] != null) {
-      bot.sendMessage(chatId, 'Отмена поиска');
+      bot.sendMessage(chatId, 'Отмена поиска', LowwerKeyboard);
       CurrentTask[chatId] = null;
     }
     if(Group[text] != undefined & CurrentTask[chatId] == 1){
@@ -61,6 +66,10 @@ const start = () => {
     if(Group[text] == undefined & CurrentTask[chatId] == 1){
       return bot.sendMessage(chatId, `Группа не найдена, проверьте корректно ли указано название, пример - ОП-13-37`)
     }
+    if (text === `/test`) {
+      //console.log('запрос получен, попытка обработки')
+      bot.sendMessage(chatId, 'try new keyboard', LowwerKeyboard)
+    }
 /*     else{
       bot.sendMessage(chatId, 'Я не совсем понял что ты хочешь от меня')
     } */
@@ -70,98 +79,93 @@ const start = () => {
     const chatId = msg.message.chat.id;
     const msgid = msg.message.message_id;
 
-/*     if(typeof uri === 'undefined'){
-      console.error('Undefined is not a valid uri or options object.')
-    }
- */
     try {
       rp(gruppa[chatId])
 
-    //rp(gruppa[chatId])
-    .then(function(html){
-      //Получилось!
-    const $ = Cheerio.load(html)
+      .then(function(html){
+          //Получилось!
+        const $ = Cheerio.load(html)
 
-    function normalizeWS(s) {
-      s = s.match(/\S+/g);
-      return s ? s.join(' ') : '';
+        function normalizeWS(s) {
+          s = s.match(/\S+/g);
+          return s ? s.join(' ') : '';
+        }
+        //чистим от пробелов
+
+        function DurationOfThePair(time){
+          let curtime = time
+          let minute;
+          let hour;
+          let endtime;
+          if(curtime.length === 5){
+            hour = curtime[0] + curtime[1];
+            minute = curtime[3] + curtime[4];
+          }
+          else{
+            hour = curtime[0];
+            minute = curtime[2] + curtime[3];
+          };
+          let NumberMinute = Number(minute) + 30;
+          let NumberHour = Number(hour);
+          if(NumberMinute>=60){
+            NumberHour = NumberHour + 1;
+            NumberMinute = NumberMinute - 60;
+          }
+          if(NumberMinute < 10){
+            minute = `0` + String(NumberMinute);
+            endtime = NumberHour + `:` + minute;
+          }
+          else{
+            endtime = NumberHour + ":" + NumberMinute;
+          }
+          return endtime;
+        }
+
+        let divDAY = data * 2
+        let h2DAY = divDAY - 1
+        let arraypara = []
+        let schedule = []
+
+        let DayOfTheWeek = normalizeWS($(`div.news-list > h2:nth-child(${h2DAY})`, html).text())
+        //запрос названия дня h2 должен быть не чётным 1 - понедельник
+
+        let arrayLen = normalizeWS($(`div:nth-child(${divDAY}) > table > tbody > tr:last > td:first`, html).text())
+        // узнаём колличество пар в день
+
+        for(let i = 1; i-1 < arrayLen; i++){
+          //запрашиваем по одному предмету в день
+          let para = i + `. `
+          for(let k = 2; k < 6; k++){
+            //запрашиваем каждый элемент предмета - номер пары, кто ведёт, что ведёт и когда
+            let paraDAY = $(`div:nth-child(${divDAY}) > table > tbody > tr:nth-child(${i}) > td:nth-child(${k})`, html).text();
+            k === 2 ? para = para + normalizeWS(paraDAY) + ` - ` + DurationOfThePair(normalizeWS(paraDAY)) + `\n` : 
+            k === 5 ? para = para + `\n` + normalizeWS(paraDAY) + `\n` : para = para + normalizeWS(paraDAY) + ` `;
+            // переносим имя препода на новую строку
+          }
+          arraypara[i-1] = para
+        }
+        
+
+        arraypara.unshift(DayOfTheWeek)
+
+        for(let v = 0; v < arraypara.length; v++){
+          schedule += arraypara[v] + `\n`
+        }
+        // преобразовываем массив в текст
+
+        bot.sendMessage(chatId, schedule, {parse_mode: 'HTML'})
+
+        let array2 = $('div:nth-child(6) > table > tbody > tr:nth-child(1) > td', html).text()
+          // tr - регулирует номер пары по порядку
+          // div - отвечает за день и должен быть чётным начиная с 2
+
+      })
+      .catch(function(err){
+        console.log(err)
+      });
     }
-    //чистим от пробелов
-
-    function DurationOfThePair(time){
-      let curtime = time
-      let minute;
-      let hour;
-      let endtime;
-      if(curtime.length === 5){
-        hour = curtime[0] + curtime[1];
-        minute = curtime[3] + curtime[4];
-      }
-      else{
-        hour = curtime[0];
-        minute = curtime[2] + curtime[3];
-      };
-      let NumberMinute = Number(minute) + 30;
-      let NumberHour = Number(hour);
-      if(NumberMinute>=60){
-        NumberHour = NumberHour + 1;
-        NumberMinute = NumberMinute - 60;
-      }
-      if(NumberMinute === 0 || NumberMinute === 5){
-        minute = `0` + String(NumberMinute);
-        endtime = NumberHour + `:` + minute;
-      }
-      else{
-        endtime = NumberHour + ":" + NumberMinute;
-      }
-      return endtime;
-    }
-
-    let divDAY = data * 2
-    let h2DAY = divDAY - 1
-    let arraypara = []
-    let schedule = []
-
-    let DayOfTheWeek = normalizeWS($(`div.news-list > h2:nth-child(${h2DAY})`, html).text())
-    //запрос названия дня h2 должен быть не чётным 1 - понедельник
-
-    let arrayLen = normalizeWS($(`div:nth-child(${divDAY}) > table > tbody > tr:last > td:first`, html).text())
-    // узнаём колличество пар в день
-
-    for(let i = 1; i-1 < arrayLen; i++){
-      //запрашиваем по одному предмету в день
-      let para = i + `. `
-      for(let k = 2; k < 6; k++){
-        //запрашиваем каждый элемент предмета - номер пары, кто ведёт, что ведёт и когда
-        let paraDAY = $(`div:nth-child(${divDAY}) > table > tbody > tr:nth-child(${i}) > td:nth-child(${k})`, html).text();
-        k === 2 ? para = para + normalizeWS(paraDAY) + ` - ` + DurationOfThePair(normalizeWS(paraDAY)) + `\n` : 
-        k === 5 ? para = para + `\n` + normalizeWS(paraDAY) + `\n` : para = para + normalizeWS(paraDAY) + ` `;
-        // переносим имя препода на новую строку
-
-      }
-      //arraypara[i-1] = normalizeWS(para)
-      arraypara[i-1] = para
-      //console.log(para.length)
-    }
-    
-
-    arraypara.unshift(DayOfTheWeek)
-
-    for(let v = 0; v < arraypara.length; v++){
-      schedule += arraypara[v] + `\n`
-    }
-    // преобразовываем массив в текст
-
-    bot.sendMessage(chatId, schedule, {parse_mode: 'HTML'})
-
-    let array2 = $('div:nth-child(6) > table > tbody > tr:nth-child(1) > td', html).text()
-      // tr - регулирует номер пары по порядку
-      // div - отвечает за день и должен быть чётным начиная с 2
-
-    })}
     catch(error){
       bot.sendMessage(chatId, 'Пожалуйста, не используйте старый запрос расписания')
-
     }
 
   });
--- options.js
+++ options.js
@@ -10,4 +10,33 @@ module.exports = {
             ]
         })
     },
+    LowwerKeyboard: {
+        reply_markup: JSON.stringify({
+            keyboard: [
+                ['Рассписание','Преподователи']
+            ],
+            resize_keyboard: true,
+            one_time_keyboard: true,
+            //force_reply: true,
+        })
+    },
+    TeacherList:{
+        reply_markup: JSON.stringify({
+            inline_keyboard:[
+                [{text: 'ghtgh', callback_data: '6'}],
+                [{text: 'ghtgh', callback_data: '7'}],
+                [{text: 'ghtgh', callback_data: '8'}],
+                [{text: 'ghtgh', callback_data: '9'}],
+                [{text: 'ghtgh', callback_data: '10'}],
+                [{text: 'ghtgh', callback_data: '11'}],
+                [{text: 'ghtgh', callback_data: '12'}],
+                [{text: 'ghtgh', callback_data: '13'}],
+                [{text: 'ghtgh', callback_data: '14'}],
+                [{text: 'ghtgh', callback_data: '15'}],
+                [{text: 'ghtgh', callback_data: '16'}],
+                [{text: 'ghtgh', callback_data: '17'}],
+                [{text: 'ghtgh', callback_data: '18'}],
+            ]
+        })
+    }
 }
